0d094f7456c113ac13e0d236d746b094127f89d8
wiraphat <saveffer01@gmail.com>
2025-06-22 20:30:11 +0700

    add 03

---
+++ 03-max-to-min/src/index.ts
@@ -0,0 +1,104 @@
+export class NumberArray {
+    private data: number[];
+    private isSorted: boolean = false;
+
+    constructor(data: number[]) {
+        if (!Array.isArray(data) || !data.every(Number.isInteger)) {
+            throw new Error('Invalid input: must be an array of integers');
+        }
+        this.data = data;
+    }
+
+    private merge(left: number[], right: number[]): number[] {
+        let resultArray: number[] = [], leftIndex = 0, rightIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex]! > right[rightIndex]!) {
+                resultArray.push(left[leftIndex]!);
+                leftIndex++;
+            } else {
+                resultArray.push(right[rightIndex]!);
+                rightIndex++;
+            }
+        }
+
+        return resultArray
+                .concat(left.slice(leftIndex))
+                .concat(right.slice(rightIndex));
+    }
+
+    private mergeSort(arr: number[]): number[] {
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        const mid = Math.floor(arr.length / 2);
+        const left = this.mergeSort(arr.slice(0, mid));
+        const right = this.mergeSort(arr.slice(mid));
+
+        return this.merge(right, left);
+    }
+
+    sortMaxToMin(): string {
+        if (this.data.length === 0) {
+            return '';
+        }
+        if (this.isSorted) {
+            return this.data.join(' ');
+        }
+        this.data = this.mergeSort(this.data);
+        this.isSorted = true;
+        return this.data.join(' ');
+    }
+
+    div(): string {
+        if (this.data.length === 0) {
+            throw new Error('Array is empty, cannot compute max/min');
+        }
+
+        if (this.isSorted) {
+            if (this.data[this.data.length - 1] === 0) {
+                throw new Error('Division by zero is not allowed');
+            }
+            const value =  this.data[0]! / this.data[this.data.length - 1]!;
+            return `${value.toFixed(2)}`;
+        } else {
+            const max = Math.max(...this.data);
+            const min = Math.min(...this.data);
+            if (min === 0) {
+                throw new Error('Division by zero is not allowed');
+            }
+            const value = Math.ceil(max / min);
+            return `${value.toFixed(2)}`;
+        }
+    }
+}
+
+
+function main() {
+    const input = prompt('Enter postive integer separated by a space:');
+    if (input === null || input.trim() === '') {
+    console.error('No input provided. Exiting.');
+    return;
+    }
+
+    const tokens = input.trim().split(/\s+/);
+
+    for (const t of tokens) {
+        if (!/^\d+$/.test(t)) {
+            console.error(`Invalid input "${t}". Only positive integers are allowed.`);
+            return;
+        }
+    }
+
+    const data: number[] = tokens.map(Number);
+
+    const numberArray = new NumberArray(data);
+    const sortedData = numberArray.sortMaxToMin();
+    console.log('Sorting (Max-to-Min):', sortedData);
+    console.log('Max/Min:', numberArray.div());
+} 
+
+if (import.meta.main) {
+  main();
+}
---
+++ 03-max-to-min/src/tests/index.spec.ts
@@ -0,0 +1,1 @@
+import { describe, test, spyOn, expect } from "bun:test";
